513741838976dbdf9ccf5af1d180c0850f021a6f
sivchari <shibuuuu5@gmail.com>
2025-05-29 16:58:58 +0900

    add Map and Filter, then add Collect for Seq
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

---
+++ it.go
@@ -0,0 +1,71 @@
+package it
+
+import (
+	"iter"
+	"slices"
+)
+
+type Seq[V any] iter.Seq[V]
+
+func NewSeq[V any](seq iter.Seq[V]) Seq[V] {
+	return Seq[V](seq)
+}
+
+func (s Seq[V]) Seq() iter.Seq[V] {
+	return iter.Seq[V](s)
+}
+
+func (s Seq[V]) Filter(f func(V) bool) Seq[V] {
+	return func(yield func(V) bool) {
+		for v := range s.Seq() {
+			if f(v) && !yield(v) {
+				return
+			}
+		}
+	}
+}
+
+func (s Seq[V]) Map(f func(V) V) Seq[V] {
+	return func(yield func(V) bool) {
+		for v := range s.Seq() {
+			if !yield(f(v)) {
+				return
+			}
+		}
+	}
+}
+
+func (s Seq[V]) Collect() []V {
+	return slices.Collect(s.Seq())
+}
+
+type Seq2[K, V any] iter.Seq2[K, V]
+
+func NewSeq2[K, V any](seq iter.Seq2[K, V]) Seq2[K, V] {
+	return Seq2[K, V](seq)
+}
+
+func (s Seq2[K, V]) Seq2() iter.Seq2[K, V] {
+	return iter.Seq2[K, V](s)
+}
+
+func (s Seq2[K, V]) Filter(f func(K, V) bool) Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range s.Seq2() {
+			if f(k, v) && !yield(k, v) {
+				return
+			}
+		}
+	}
+}
+
+func (s Seq2[K, V]) Map(f func(K, V) (K, V)) Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range s.Seq2() {
+			newK, newV := f(k, v)
+			if !yield(newK, newV) {
+				return
+			}
+		}
+	}
+}
